888680d4e41b606c1107780997aea88817b1b9c2
anggakswr <222180023@student.unaki.ac.id>
2020-10-12 16:30:07 +0700

    menambah validasi di form TambahBarang.js, mau nambah brg di db tp blm berhasil

---
+++ config/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Set the configuration for your app
+const config = {
+  apiKey: "AIzaSyAHjM45qfMUOr7IwEEchFjl2eZkVOFzjrY",
+  authDomain: "inventoryku-20cbd.firebaseapp.com",
+  databaseURL: "https://inventoryku-20cbd.firebaseio.com",
+  storageBucket: "inventoryku-20cbd.appspot.com",
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+// const database = firebase.database();
+
+export default firebase;
--- screens/TambahBarang.js
+++ screens/TambahBarang.js
@@ -1,56 +1,119 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { TextInput, Button } from "react-native-paper";
+import { View, ScrollView, StyleSheet, Text } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+import { useForm, Controller } from "react-hook-form";
 import globalStyles from "../styles/globalStyles";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import firebase from "../config/firebase";
 
 const TambahBarang = () => {
-  const [submit, setSubmit] = React.useState({
-    nama: "",
-    stok: "",
-    hargaBeli: "",
-    hargaJual: "",
-  });
+  const { control, handleSubmit, errors } = useForm();
 
-  console.log(submit);
+  // menambah brg ke db
+  const addBarang = (data) => {
+    const db = firebase.firestore();
+    db.collection("barangs")
+      .add(data)
+      .then(() => console.log("barang added"))
+      .catch((err) => console.log("add barang failed", err));
+  };
 
   return (
-    <>
-      <View style={globalStyles.whiteContainer}>
-        <TextInput
-          label="Nama barang"
-          value={submit.nama}
-          onChangeText={(text) => setSubmit({ ...submit, nama: text })}
-        />
+    <ScrollView>
+      {inputs.map((input, i) => (
+        <View key={i.toString()}>
+          {/* dr react hook form */}
+          <Controller
+            control={control}
+            render={({ onChange, onBlur, value }) => (
+              <View style={[globalStyles.whiteContainer, globalStyles.flexRow]}>
+                <MaterialCommunityIcons
+                  name={input.icon}
+                  size={30}
+                  color="grey"
+                  style={styles.inputIcon}
+                />
 
-        <TextInput
-          label="Stok"
-          value={submit.stok}
-          onChangeText={(text) => setSubmit({ ...submit, stok: text })}
-          keyboardType="numeric"
-        />
+                {/* component buatan sendiri */}
+                <TextInput
+                  label={input.label}
+                  onBlur={onBlur}
+                  onChangeText={(value) => onChange(value)}
+                  value={value}
+                  style={styles.inputStyle}
+                  keyboardType={input.keyboardType ? "default" : "numeric"}
+                />
+              </View>
+            )}
+            name={input.name}
+            rules={
+              input.rules
+                ? { required: true }
+                : { required: true, pattern: /^[1-9]\d*$/g }
+            }
+            defaultValue=""
+          />
 
-        <TextInput
-          label="Harga beli"
-          value={submit.hargaBeli}
-          onChangeText={(text) => setSubmit({ ...submit, hargaBeli: text })}
-          keyboardType="numeric"
-        />
+          {/* error */}
+          {errors[input.name] && (
+            <Text style={styles.error}>
+              Jumlah barang harus diisi dengan benar.
+            </Text>
+          )}
+        </View>
+      ))}
 
-        <TextInput
-          label="Harga jual"
-          value={submit.hargaJual}
-          onChangeText={(text) => setSubmit({ ...submit, hargaJual: text })}
-          keyboardType="numeric"
-        />
-      </View>
-
-      <View style={{ ...globalStyles.whiteContainer, paddingHorizontal: 20 }}>
-        <Button mode="contained" onPress={() => console.log("Tambah")}>
+      <View style={{ padding: 20 }}>
+        <Button mode="contained" onPress={handleSubmit(addBarang)}>
           Simpan
         </Button>
       </View>
-    </>
+    </ScrollView>
   );
 };
+
+// style
+const styles = StyleSheet.create({
+  inputStyle: {
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+  inputIcon: {
+    padding: 15.5,
+    backgroundColor: "#fff",
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgrey",
+  },
+  error: {
+    color: "red",
+    marginLeft: 20,
+  },
+});
+
+// input field
+const inputs = [
+  {
+    label: "Nama barang",
+    name: "namaBarang",
+    icon: "cube",
+    rules: true,
+    keyboardType: true,
+  },
+  {
+    label: "Harga beli",
+    name: "hargaBeli",
+    icon: "cash-multiple",
+  },
+  {
+    label: "Harga jual",
+    name: "hargaJual",
+    icon: "cash-multiple",
+  },
+  {
+    label: "Stok",
+    name: "stok",
+    icon: "database",
+  },
+];
 
 export default TambahBarang;
