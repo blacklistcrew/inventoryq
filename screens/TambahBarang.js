6c66fbcc9acaee595a8703c4b97fd56a95f46046
anggakswr <222180023@student.unaki.ac.id>
2020-10-21 17:19:03 +0700

    menambah field satuan

--- screens/TambahBarang.js
+++ screens/TambahBarang.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import {View, ScrollView, Text} from 'react-native';
-import {Button, TextInput, Snackbar} from 'react-native-paper';
+import React, {useState, useReducer} from 'react';
+import {View, ScrollView, Text, TextInput} from 'react-native';
+import {
+  Button,
+  Snackbar,
+  TouchableRipple,
+  Portal,
+  Modal,
+} from 'react-native-paper';
 import {useForm, Controller} from 'react-hook-form';
 import globalStyles from '../styles/globalStyles';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import firestore from '@react-native-firebase/firestore';
 
+const initSatuan = 'Pcs';
+const satuanReducer = (state = initSatuan, action) => {
+  switch (action.type) {
+    case 'CHANGE_SATUAN':
+      return action.satuan;
+
+    default:
+      return state;
+  }
+};
+
 const TambahBarang = ({navigation}) => {
+  // react-hook-form
   const {control, handleSubmit, errors, reset} = useForm();
-  const [notif, setNotif] = React.useState(false);
+  // snackbar
+  const [notif, setNotif] = useState(false);
   const toggleNotif = () => setNotif(!notif);
   const dismissNotif = () => setNotif(false);
+  // satuan
+  const [stateSatuan, dispatchSatuan] = useReducer(satuanReducer, initSatuan);
 
   // menambah brg ke db
   const addBarang = ({namaBrg, hargaBeli, hargaJual}) => {
@@ -20,6 +41,7 @@ const TambahBarang = ({navigation}) => {
         namaBrg,
         hargaBeli: parseInt(hargaBeli),
         hargaJual: parseInt(hargaJual),
+        satuan: stateSatuan,
         stok: 0,
         createdAt: new Date(),
       })
@@ -52,11 +74,11 @@ const TambahBarang = ({navigation}) => {
 
                 {/* component buatan sendiri */}
                 <TextInput
-                  label={input.label}
+                  placeholder={input.label}
                   onBlur={onBlur}
                   onChangeText={(value) => onChange(value)}
                   value={value}
-                  style={styles.inputStyle}
+                  style={styles.textboxCari}
                   keyboardType={input.keyboardType ? 'default' : 'numeric'}
                   autoCapitalize="words"
                 />
@@ -82,13 +104,17 @@ const TambahBarang = ({navigation}) => {
         </View>
       ))}
 
+      {/* satuan */}
+      <Satuan stateSatuan={stateSatuan} dispatchSatuan={dispatchSatuan} />
+
+      {/* tombol simpan */}
       <View style={{padding: 20}}>
         <Button mode="contained" onPress={handleSubmit(addBarang)}>
           Simpan
         </Button>
       </View>
 
-      {/* Notif */}
+      {/* notif */}
       <Snackbar
         visible={notif}
         onDismiss={dismissNotif}
@@ -104,8 +130,11 @@ const TambahBarang = ({navigation}) => {
 
 // style
 const styles = {
-  inputStyle: {
-    backgroundColor: '#fff',
+  textboxCari: {
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    margin: 10,
+    paddingHorizontal: 20,
     flex: 1,
   },
   inputIcon: {
@@ -115,6 +144,19 @@ const styles = {
   error: {
     color: 'red',
     marginLeft: 20,
+  },
+  smallModal: {
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 10,
+  },
+  sortbyItem: {
+    minHeight: 50,
+    minWidth: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 };
 
@@ -138,6 +180,67 @@ const inputs = [
     name: 'hargaJual',
     icon: 'cash-multiple',
   },
+];
+
+const Satuan = ({stateSatuan, dispatchSatuan}) => {
+  // modal
+  const [visible, setVisible] = useState(false);
+  const toggleModal = () => setVisible(!visible);
+
+  return (
+    <View style={[globalStyles.whiteContainer, globalStyles.flexRow]}>
+      {/* simbol coin */}
+      <MaterialCommunityIcons
+        name="database"
+        size={30}
+        color="grey"
+        style={styles.inputIcon}
+      />
+
+      <TouchableRipple
+        rippleColor="rgba(0, 0, 0, .32)"
+        onPress={toggleModal}
+        style={{...styles.textboxCari, flex: 1, padding: 15}}>
+        <View style={globalStyles.flexRow}>
+          {/* tulisan */}
+          <Text style={{color: 'grey', flex: 1}}>{stateSatuan}</Text>
+          {/* icon panah bawah */}
+          <MaterialCommunityIcons name="chevron-down" color="grey" />
+        </View>
+      </TouchableRipple>
+
+      {/* modal */}
+      <Portal>
+        <Modal visible={visible} onDismiss={toggleModal}>
+          <View style={styles.smallModal}>
+            {satuanList.map((item, i) => (
+              <TouchableRipple
+                onPress={() => {
+                  dispatchSatuan({type: 'CHANGE_SATUAN', satuan: item});
+                  toggleModal();
+                }}
+                rippleColor="rgba(0, 0, 0, .32)"
+                style={styles.sortbyItem}
+                key={i.toString()}>
+                <Text>{item}</Text>
+              </TouchableRipple>
+            ))}
+          </View>
+        </Modal>
+      </Portal>
+    </View>
+  );
+};
+
+const satuanList = [
+  'Pcs',
+  'Kg',
+  'Liter',
+  'Botol',
+  'Kardus',
+  'Unit',
+  'Buah',
+  'Meter',
 ];
 
 export default TambahBarang;
